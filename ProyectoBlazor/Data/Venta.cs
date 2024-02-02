c731b5fd5c13078e8cb5eb45da62092a6256929d
Alanna <2021-0170@uteco.edu.do>
2024-02-02 19:17:12 -0400

    se creo los componentes  vblazor

---
+++ ProyectoBlazor/Data/Venta.cs
@@ -0,0 +1,9 @@
+﻿namespace ProyectoBlazor.Data
+{
+    public class Venta
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public string Foto { get; set; } = string.Empty;
+    }
+}
