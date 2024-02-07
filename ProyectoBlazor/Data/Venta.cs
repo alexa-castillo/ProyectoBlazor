d4b6e37e6025cba790c2c278089d40afe7ae676c
alexa-castillo <156859142+alexa-castillo@users.noreply.github.com>
2024-02-07 18:28:16 -0400

    Se hizo una pagina para saludar

--- ProyectoBlazor/Data/Venta.cs
+++ ProyectoBlazor/Data/Venta.cs
@@ -1,9 +1,9 @@
-﻿namespace ProyectoBlazor.Data
+﻿
+namespace ProyectoBlazor.Data
 {
     public class Venta
     {
-        public string Nombre { get; set; } = string.Empty;
-        public string Descripcion { get; set; } = string.Empty;
-        public string Foto { get; set; } = string.Empty;
+        public required string Nombre { get; set; }
     }
 }
+
